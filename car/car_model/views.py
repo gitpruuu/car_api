6f308a4c18cff2d7122549d5e5556c0046d10755
Johnatan Souza <jgs8115@gmail.com>
2021-05-07 20:51:00 -0300

    Adding car_model

--- car/car_model/views.py
+++ car/car_model/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, permissions
+from car.car_model.serializers import CarSerializer, CarModelSerializer, UserSerializer
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class CarViewSet(viewsets.ModelViewSet):
+    
+    queryset = Group.objects.all()
+    serializer_class = CarSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class CarModelViewSet(viewsets.ModelViewSet):
+    
+    queryset = Group.objects.all()
+    serializer_class = CarModelSerializer
+    permission_classes = [permissions.IsAuthenticated]
